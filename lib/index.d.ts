c94bc094c6ac8c4c6b4fb7b85e642d98f1384f17
John Kenny <johnkenny54@yahoo.com>
2024-12-20 19:16:53 -0800

    fix: class attribute name

---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+export type Root = import("hast").Root;
+export type Element = import("hast").Element;
+export type Text = import("hast").Text;
+
+export class htmlScrape {
+    static getAttr(element: Element, attName: string): string | undefined;
+    static getSubtree(
+        ast: Root | Element,
+        fnMatch: (element: Element) => boolean,
+    ): Element | undefined;
+    static getSubtrees(
+        element: Root | Element,
+        fnMatch: (element: Element) => boolean,
+    ): Element[];
+    static getTextContent(node: Element | Text): string;
+    static parseFile(filePath: string): Root;
+}
